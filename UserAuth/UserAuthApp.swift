e75f8a9e4ab02e30d88c0ea58f8ce1568d16750c
jules <jules.menelas@hiddengeniusproject.org>
2024-06-11 10:33:18 -0400

    Initial Commit

---
+++ UserAuth/UserAuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserAuthApp.swift
+//  UserAuth
+//
+//  Created by Jules Menelas on 6/11/24.
+//
+
+import SwiftUI
+
+@main
+struct UserAuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
